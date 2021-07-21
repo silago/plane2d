afb689ff96bfd72c4a44c9d59db19e7584526344
silago nevermindd <silago.nevermind@gmail.com>
2021-07-21 22:18:12 +0300

    enemy behaviour

--- Assets/Modules/PlaneSettings/PlaneSettings.cs
+++ Assets/Modules/PlaneSettings/PlaneSettings.cs
@@ -5,7 +5,6 @@ namespace Modules.PlaneSettings
     [CreateAssetMenu(menuName = "Planes/CommonSettings", fileName = "CommonPlaneSettings", order = 0)]
     public class PlaneSettings : ScriptableObject
     {
-        public AttackType attackType;
         public Utils.MinMax attackEvery;
         public float angleTreshhold = 1f;
         public float desiredDistance;
